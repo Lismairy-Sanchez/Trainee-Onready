3176cc15d3050fd96b728f1a4230355c7bab6fc0
Alejandra <sanchezlismairy@gmail.com>
2020-12-29 01:25:06 -0300

    Clase Autos

--- Clases/Autos.js
+++ Clases/Autos.js
@@ -1,7 +1,8 @@
+// Import
 const Vehiculos = require("./Vehiculos");
 
+//Clase Auto extiende de la clase Vehiculos
 class Autos extends Vehiculos {
-    //Clase Auto se extiende de la clase Vehiculos
     constructor(marca, modelo, precio, puertas) {
         super(marca, modelo, precio); //Llamo funciones del objeto padre
         this.puertas = puertas;
