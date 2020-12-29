41baafd739c0d7286144802c184c3e82442f218a
Alejandra <sanchezlismairy@gmail.com>
2020-12-29 01:37:25 -0300

    Clase Moto

--- Diagrama_Clases/Motos.js
+++ Diagrama_Clases/Motos.js
@@ -2,10 +2,10 @@
 const Vehiculos = require("./Vehiculos");
 
 //Clase Motos extiende de la clase Vehiculos
-class Motos extends Vehiculos {
+class Moto extends Vehiculos {
     constructor(Marca, Modelo, Precio, Cilindrada) {
         super(Marca, Modelo, Precio); //Llamo funciones del objeto padre
         this.Cilindrada = Cilindrada;
     }
 }
-module.exports = Motos;
+module.exports = Moto;
