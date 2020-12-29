a121c65ac85f5bbf16322d86434218369584a309
Alejandra <sanchezlismairy@gmail.com>
2020-12-29 01:18:10 -0300

    Clase Vehiculos

---
+++ Clases/Vehiculos.js
@@ -0,0 +1,9 @@
+class Vehiculos {
+    constructor(Marca, Modelo, Precio) {
+        this.Marca = Marca;
+        this.Modelo = Modelo;
+        this.Precio = Precio;
+    }
+}
+
+module.exports = Vehiculos;
